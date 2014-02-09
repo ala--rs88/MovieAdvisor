11df8e7c05c200ff10c123c3f253511ad339ddc0
Igor Karpov <igork.fet@gmail.com>
2014-02-09 23:30:44 +0300

    -- raw datasets added; -- solution created; -- empty bulk data loader project created.

---
+++ MovieAdvisor/MovieAdvisor.BulkDataLoader/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieAdvisor.BulkDataLoader
+{
+    // todo: consider renaming
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, user!");
+        }
+    }
+}
